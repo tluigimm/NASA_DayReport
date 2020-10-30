cede36f6cfa06cae6e19559a02b1e92f4a787d73
tluigimm <tluigimm@tutanota.com>
2020-10-30 19:07:41 -0300

    NeoWs in front-end

--- client/src/App.js
+++ client/src/App.js
@@ -10,16 +10,37 @@ function App() {
   const [urlImage, setUrlImage] = useState('');
   const [imageTitle, setImageTitle] = useState('');
   const [imageText, setImageText] = useState('');
+  const [neoList, setNeoList] = useState('');
+  let neos = [];
 
   const sendDate = () => {
     Axios.post('http://localhost:7002/send_date', {
       d: date
     }).then((res) => {
+      var neoJson = res.data.resNeows.near_earth_objects;
+      for (var key in neoJson) {
+        setNeoList(neoJson[key])
+      }
+
       setUrlImage(res.data.resApod.url);
       setImageTitle(res.data.resApod.title);
       setImageText(res.data.resApod.explanation);
     });
   };
+
+  
+  for (let neo of neoList) {
+    neos.push(<div>
+      name: {neo.name} <br/>
+      average estimated diameter (m): {(neo.estimated_diameter.meters.estimated_diameter_min 
+                                         + neo.estimated_diameter.meters.estimated_diameter_max) / 2}<br/>
+      relative velocity (km/s): {neo.close_approach_data[0].relative_velocity.kilometers_per_second}<br/>
+      miss distance (astronomical): {neo.close_approach_data[0].miss_distance.astronomical}<br/>
+      orbiting body: {neo.close_approach_data[0].orbiting_body}<br/>
+      absolute magnitude (h): {neo.absolute_magnitude_h}<br/><br/>
+    </div>
+    );
+  }
 
   return (
     <div className="App">
@@ -30,11 +51,12 @@ function App() {
         setDate(e.target.value)
       }}/>
       <button onClick={sendDate}> send </button>
-      <center>
+      <br/><center>
         <h3>{imageTitle}</h3>
         <img src={urlImage}/>
         <p>{imageText}</p>
-      </center>
+      </center> <br/>
+      {neos}
     </div>
   );
 }
